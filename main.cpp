fe98d67f760a0e88cb45b0ebedb9e9453d40d496
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-12-08 20:00:20 +0100

    Adding all the necessary files for the project

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
-using namespace std;
-
 int		main(void)
 {
-	cout << "Testing and everything is working fine" << "\n";
+	std::cout << "Testing and everything is working fine" << "\n";
 	return (0);
 }
